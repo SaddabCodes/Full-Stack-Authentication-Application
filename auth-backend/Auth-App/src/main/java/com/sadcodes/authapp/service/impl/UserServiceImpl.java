3ed0d7fa2776cd9d9bc963b5442bc2bdec16b01f
saddab <saddabakhtar17@gmail.com>
2026-01-05 23:59:02 +0530

    Add ModelMapper dependency

--- auth-backend/Auth-App/src/main/java/com/sadcodes/authapp/service/impl/UserServiceImpl.java
+++ auth-backend/Auth-App/src/main/java/com/sadcodes/authapp/service/impl/UserServiceImpl.java
@@ -2,10 +2,15 @@ package com.sadcodes.authapp.service.impl;
 
 import com.sadcodes.authapp.dto.UserDto;
 import com.sadcodes.authapp.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
+
+    private final UserService userService;
+
     @Override
     public UserDto createUser(UserDto userDto) {
         return null;
