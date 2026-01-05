af8cf0926af5a68269c5e653c5ad2c0c39f1948d
saddab <saddabakhtar17@gmail.com>
2026-01-06 00:02:03 +0530

    Configure ModelMapper bean for DTO mapping

--- auth-backend/Auth-App/src/main/java/com/sadcodes/authapp/service/impl/UserServiceImpl.java
+++ auth-backend/Auth-App/src/main/java/com/sadcodes/authapp/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.sadcodes.authapp.service.impl;
 
+import com.sadcodes.authapp.config.ProjectConfig;
 import com.sadcodes.authapp.dto.UserDto;
 import com.sadcodes.authapp.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
 
     private final UserService userService;
+    private final ProjectConfig projectConfig;
 
     @Override
     public UserDto createUser(UserDto userDto) {
