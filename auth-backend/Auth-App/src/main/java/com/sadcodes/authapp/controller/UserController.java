f3c1fa34047ffd5ac33ec4dcd9585ea2bc973890
saddab <saddabakhtar17@gmail.com>
2026-01-08 23:50:56 +0530

    build createUser method in controller class

--- auth-backend/Auth-App/src/main/java/com/sadcodes/authapp/controller/UserController.java
+++ auth-backend/Auth-App/src/main/java/com/sadcodes/authapp/controller/UserController.java
@@ -1,4 +1,24 @@
 package com.sadcodes.authapp.controller;
 
+import com.sadcodes.authapp.dto.UserDto;
+import com.sadcodes.authapp.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/users")
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserDto>createUser(@RequestBody UserDto userDto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(userDto));
+    }
 }
