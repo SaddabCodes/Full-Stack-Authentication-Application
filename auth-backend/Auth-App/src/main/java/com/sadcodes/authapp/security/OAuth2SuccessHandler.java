8f4fe26da70cf6d14037b24d0849b4f9fb1211d2
saddab <saddabakhtar17@gmail.com>
2026-02-11 23:41:35 +0530

    Complete Google OAuth2 authentication integration

--- auth-backend/Auth-App/src/main/java/com/sadcodes/authapp/security/OAuth2SuccessHandler.java
+++ auth-backend/Auth-App/src/main/java/com/sadcodes/authapp/security/OAuth2SuccessHandler.java
@@ -1,8 +1,11 @@
 package com.sadcodes.authapp.security;
 
 import com.sadcodes.authapp.entities.Provider;
+import com.sadcodes.authapp.entities.RefreshToken;
 import com.sadcodes.authapp.entities.User;
+import com.sadcodes.authapp.repository.RefreshTokenRepository;
 import com.sadcodes.authapp.repository.UserRepository;
+import com.sadcodes.authapp.service.CookieService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -16,6 +19,9 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.rmi.RemoteException;
+import java.time.Instant;
+import java.util.UUID;
 
 @AllArgsConstructor
 @Component
@@ -23,6 +29,9 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final CookieService cookieService;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
@@ -48,34 +57,38 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
         switch (registrationId) {
 
             case "google" -> {
-                String googleId = oAuth2User.getAttribute("sub");
                 String email = oAuth2User.getAttribute("email");
                 String name = oAuth2User.getAttribute("name");
-                String picture = oAuth2User.getAttribute("picture");
+                String image = oAuth2User.getAttribute("picture");
 
-                user = User.builder()
-                        .email(email)
-                        .name(name)
-                        .image(picture)
-                        .provider(Provider.GOOGLE)
-                        .providerId(googleId)
-                        .build();
+                user = userRepository.findByEmail(email).orElseGet(() -> {
+                    User newUser = User.builder()
+                            .email(email)
+                            .name(name)
+                            .image(image)
+                            .provider(Provider.GOOGLE)
+                            .build();
+                    return userRepository.save(newUser);
+                });
 
-                userRepository.findByEmail(email)
-                        .ifPresentOrElse(
-                                existingUser -> {
-                                    logger.info("User already exists: " + existingUser);
-                                },
-                                () -> {
-                                    logger.info("Saving new user");
-                                    userRepository.save(user);
-                                }
-                        );
             }
 
-            default -> logger.info("Unknown provider");
+            default -> {
+                throw new RemoteException("Invalid registration id");
+            }
         }
 
+
+        String jti = UUID.randomUUID().toString();
+        RefreshToken refreshTokenOb = RefreshToken.builder().jti(jti).user(user).revoked(false).createdAt(Instant.now())
+                .expiresAt(Instant.now().plusSeconds(jwtService.getRefreshTtlSeconds())).build();
+
+        refreshTokenRepository.save(refreshTokenOb);
+
+        String accessToken = jwtService.generateAccessToken(user);
+        String refreshToken = jwtService.generateRefreshToken(user, refreshTokenOb.getJti());
+
+        cookieService.attachRefreshCookie(response, refreshToken, (int) jwtService.getAccessTtlSeconds());
         response.getWriter().write("Login successful");
     }
 
